8e2cca838ca3678f31537416a8b0dde9c2110e8c
Alexander A Cerlini <contato@alexcerlini.com.br>
2023-01-26 19:37:38 -0300

    feat: create style for SignupItem

--- src/components/SignupItem/index.tsx
+++ src/components/SignupItem/index.tsx
@@ -1,12 +1,13 @@
 import { Input, InputLabel } from "@mui/material";
 import { SignupItemProps } from "./type";
+import * as Style from "./styles";
 
 const SignupItem = ({ label, id, register }: SignupItemProps) => {
   return (
-    <>
+    <Style.Container>
       <InputLabel htmlFor={id}>{label}</InputLabel>
       <Input id={id} {...register(id)} />
-    </>
+    </Style.Container>
   );
 };
 
