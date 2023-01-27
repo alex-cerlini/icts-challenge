23594464366241d51ec11f727568d6ae417db21d
Alexander A Cerlini <contato@alexcerlini.com.br>
2023-01-27 01:52:20 -0300

    feat (Signup): create masks, validations with yup and reset function

--- src/components/Signup/index.tsx
+++ src/components/Signup/index.tsx
@@ -1,7 +1,7 @@
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
-import { Input } from "@mui/material";
+import { Box, Button } from "@mui/material";
 import { FormInputsProps } from "./type";
 import { fields } from "./data.mock";
 
@@ -9,13 +9,19 @@ import SignupItem from "../SignupItem";
 
 import * as Style from "./styles";
 
+const emailMask =
+  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+
 const schema = yup
   .object({
     fullName: yup.string().required(),
     address: yup.string().required(),
-    phone: yup.string().required(),
-    email: yup.string().required(),
-    birthDate: yup.string().required(),
+    phone: yup.number().max(99999999999).required(),
+    email: yup
+      .string()
+      .matches(emailMask, "E-mail inválido")
+      .required("Campo obrigatório"),
+    birthDate: yup.date().required("Data é obrigatório").min(new Date(0)),
   })
   .required();
 
@@ -23,20 +29,56 @@ const Signup = () => {
   const {
     register,
     handleSubmit,
+    resetField,
     formState: { errors },
   } = useForm<FormInputsProps>({
     resolver: yupResolver(schema),
   });
-  const onSubmit = (data: FormInputsProps) => console.log(data);
+
+  const onSubmit = (data: FormInputsProps) => {
+    data.fullName = data.fullName.replace(/[0-9]/g, "");
+    console.log(data);
+  };
+
+  const handleReset = () => {
+    resetField("address");
+    resetField("birthDate");
+    resetField("email");
+    resetField("fullName");
+    resetField("phone");
+  };
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <Style.Container>
         {fields?.map((field) => (
-          <SignupItem id={field.id} label={field.label} register={register} />
+          <SignupItem
+            id={field.id}
+            label={field.label}
+            register={register}
+            key={field.id}
+          />
         ))}
-
-        <Input type="submit" />
+        <Box
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            width: "50%",
+            marginTop: 18,
+          }}
+        >
+          <Button
+            variant="contained"
+            type="submit"
+            onClick={handleReset}
+            color="error"
+          >
+            Limpar
+          </Button>
+          <Button variant="contained" type="submit">
+            Enviar
+          </Button>
+        </Box>
       </Style.Container>
     </form>
   );
