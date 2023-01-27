cdafbbeac075e05985f28f237ce5fd7fe64fda06
Alexander A Cerlini <contato@alexcerlini.com.br>
2023-01-27 01:52:48 -0300

    feat (SignupItem): change type input using the id

--- src/components/SignupItem/index.tsx
+++ src/components/SignupItem/index.tsx
@@ -1,12 +1,23 @@
 import { Input, InputLabel } from "@mui/material";
 import { SignupItemProps } from "./type";
 import * as Style from "./styles";
+import { SignupEnum } from "../Signup/enum";
 
 const SignupItem = ({ label, id, register }: SignupItemProps) => {
+  const typeInput = [
+    SignupEnum.address,
+    SignupEnum.email,
+    SignupEnum.fullName,
+  ].includes(id)
+    ? "text"
+    : id === SignupEnum.birthDate
+    ? "date"
+    : "number";
+
   return (
     <Style.Container>
       <InputLabel htmlFor={id}>{label}</InputLabel>
-      <Input id={id} {...register(id)} />
+      <Input id={id} {...register(id)} type={typeInput} />
     </Style.Container>
   );
 };
